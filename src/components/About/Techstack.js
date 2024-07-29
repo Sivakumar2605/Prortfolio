8890833f7fd439903bfa55ef67165d4fd6783a06
sivakumaractimize <sivakumar@actimize.in>
2024-07-29 12:29:29 +0530

    first

---
+++ src/components/About/Techstack.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import {
+  DiHtml5,
+  DiCss3,
+  DiJavascript1,
+  DiJqueryLogo,
+  DiBootstrap,
+  DiReact,
+  DiRedux,
+  DiJava,
+} from "react-icons/di";
+import { SiMaterialdesign, SiMysql } from "react-icons/si";
+
+function Techstack() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiHtml5 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiCss3 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiJavascript1 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiJqueryLogo />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiBootstrap />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiMaterialdesign />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiReact />
+      </Col>
+      
+      <Col xs={4} md={2} className="tech-icons">
+        <DiJava />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiMysql />
+      </Col>
+    </Row>
+  );
+}
+
+export default Techstack;
---
+++ src/components/Home/Home2.js
@@ -0,0 +1,114 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import myImg from "../../Assets/avatar.svg";
+import Tilt from "react-parallax-tilt";
+import {
+  AiFillGithub,
+  AiOutlineTwitter,
+  AiFillInstagram,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+
+function Home2() {
+  return (
+    <Container fluid className="home-about-section" id="about">
+      <Container>
+        <Row>
+          <Col md={8} className="home-about-description">
+            <h1 style={{ fontSize: "2.6em" }}>
+              LET ME <span className="purple"> INTRODUCE </span> MYSELF
+            </h1>
+            <p className="home-about-body">
+              I fell in love with programming and have learned a lot along the way.
+              <br />
+              <br />I am fluent in classics like
+              <i>
+                <b className="purple"> HTML, CSS, and JavaScript </b>
+              </i>
+              <br />
+              <br />
+              My areas of interest include building new &nbsp;
+              <i>
+                <b className="purple">Web Technologies and Products </b>
+              </i>
+              as well as exploring
+              <i>
+                <b className="purple"> Blockchain</b>
+              </i>
+              <br />
+              <br />
+              I am also passionate about developing products with <b className="purple">React.js</b> and
+              <i>
+                <b className="purple"> Modern JavaScript Libraries and Frameworks</b>
+              </i>
+              , including
+              <i>
+                <b className="purple"> Redux</b>
+              </i>
+              <br />
+              <br />
+              Additionally, I am experienced with <b className="purple">Bootstrap</b> and <b className="purple">Material-UI</b> for styling, and I have a strong focus on <b className="purple">unit testing</b> with <b className="purple">Jest</b>.
+            </p>
+          </Col>
+          <Col md={4} className="myAvtar">
+            <Tilt>
+              <img src={myImg} className="img-fluid" alt="avatar" />
+            </Tilt>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12} className="home-about-social">
+            <h1>FIND ME ON</h1>
+            <p>
+              Feel free to <span className="purple">connect </span>with me
+            </p>
+            <ul className="home-about-social-links">
+              <li className="social-icons">
+                <a
+                  href="https://github.com/soumyajit4419"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiFillGithub />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://twitter.com/Soumyajit4419"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiOutlineTwitter />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.linkedin.com/in/soumyajit4419/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <FaLinkedinIn />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.instagram.com/soumyajit4419"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour home-social-icons"
+                >
+                  <AiFillInstagram />
+                </a>
+              </li>
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+
+export default Home2;
